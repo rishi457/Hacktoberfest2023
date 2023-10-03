5c98eeb9f6cd5b419b3718844ec0008e348103d9
Sidhant Mishra <69967446+rishi457@users.noreply.github.com>
2023-10-03 18:05:44 +0530

    Update HashMap.java
    Variable Name Clarity: The variable l was renamed to phoneBook to make its purpose more clear. This change enhances code readability.
    
    User Prompts: Added user prompts as comments to guide the user during input. This makes the program more user-friendly by indicating what information is expected.
    
    Newline Character Handling: After reading an integer using sc.nextInt(), there is a newline character left in the input stream. To handle this, sc.nextLine() was added to consume the newline character, ensuring that subsequent nextLine() calls correctly read user input.
    
    Code Comments: Added comments throughout the code to explain each section's purpose and functionality. This helps improve code maintainability and makes it easier for others to understand.
    
    Code Structure: The code structure was kept intact, maintaining the core functionality of creating a phone book and looking up phone numbers by name.
    
    These changes enhance the clarity, user-friendliness, and maintainability of the code, ensuring that it functions correctly and can be easily understood by both developers and users.

--- Add Code Here/HashMap - Java/HashMap.java
+++ Add Code Here/HashMap - Java/HashMap.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 import java.util.HashMap;
-class Solution{
-    public static void main(String []argh)
-    {
+
+class Solution {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<String,Integer> l = new HashMap<>();
-		System.out.println("Enter value of n: ");
+        HashMap<String, Integer> phoneBook = new HashMap<>();
+
+        System.out.println("Enter value of n: ");
         int n = sc.nextInt();
-		sc.nextLine(); //to tackle the skipped nextLine()
-		String s;
-		System.out.println("Enter values for Phone Book now");
-        for(int i=0;i<n;i++)
-        {
-			System.out.println("Enter Name: ");
-			s = sc.nextLine();
-			System.out.println("Enter Number: ");
-            l.put(s,sc.nextInt());
-            sc.nextLine(); //to tackle the skipped nextLine() 
+        sc.nextLine(); // Consume the newline character left by nextInt()
+
+        String name;
+        System.out.println("Enter values for Phone Book now");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Name: ");
+            name = sc.nextLine();
+            System.out.println("Enter Number: ");
+            int number = sc.nextInt();
+            sc.nextLine(); // Consume the newline character left by nextInt()
+            phoneBook.put(name, number);
         }
-        
-        while(sc.hasNext())
-        {
-			//System.out.println("Enter Name to be searched: ");
-            s = sc.nextLine();
-            if(l.containsKey(s)){
-                System.out.println(s+"="+l.get(s));
-            }
-            else{
+
+        while (sc.hasNext()) {
+            System.out.println("Enter Name to be searched: ");
+            name = sc.nextLine();
+            if (phoneBook.containsKey(name)) {
+                System.out.println(name + "=" + phoneBook.get(name));
+            } else {
                 System.out.println("Not found");
             }
         }
